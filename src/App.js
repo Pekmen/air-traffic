a4a83139b891be44511c20d6bfc478bd8402ab13
Pekmen <milanpekovic89@gmail.com>
2018-03-10 14:45:31 +0100

    Added react-bootstrap, basic hierarchy for pages and components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './Components/Header/Header';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <h1>Air Traffic Test</h1>
       </div>
     );
